54231b3da0c39c5c143b3ad4c0beded553d545db
Todd Shore <tcshore@kth.se>
2017-10-13 15:39:19 +0200

    now caching training data

--- tangrams_analysis/words_as_classifiers.py
+++ tangrams_analysis/words_as_classifiers.py
@@ -41,11 +41,12 @@ class CrossValidator(object):
 
 	def create_training_data(self, sessions: ItemsView[str, SessionData]):
 		for infile, session in sessions:
-			game_round_token_df = self.session_game_round_utt_factory(session)
+			try:
+				game_round_token_df = self.session_training_data[infile]
+			except KeyError:
+				game_round_token_df = self.session_game_round_utt_factory(session)
+				self.session_training_data[infile] = game_round_token_df
 			print(game_round_token_df)
-		# for round_id, (game_round, round_utts) in enumerate(session_round_utts.game_round_utts,
-		#													start=game_utterances.SessionGameRoundUtteranceFactory.ROUND_ID_OFFSET):
-		#	round_instructor_id = session_round_utts.round_instructor_ids[round_id]
 
 	def __call__(self, cross_validation_data: CrossValidationData):
 		self.create_training_data(cross_validation_data.training_data.items())
