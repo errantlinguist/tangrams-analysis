16f1d0d55a4ac0fb36ef52c24dfe72631c1d0096
errantlinguist <errantlinguist+github@gmail.com>
2017-10-15 15:16:53 +0200

    now properly finding utts even for the last round in a session

--- tangrams_analysis/game_utterances.py
+++ tangrams_analysis/game_utterances.py
@@ -1,8 +1,8 @@
 import itertools
 from enum import Enum, unique
-from numbers import Number
+from numbers import Integral, Number
 from string import ascii_uppercase
-from typing import Callable, Iterable, Iterator, List, Sequence, \
+from typing import Callable, Iterable, Iterator, Sequence, \
 	Tuple, TypeVar
 
 import numpy as np
@@ -15,17 +15,55 @@ import utterances
 N = TypeVar('N', bound=Number)
 
 
-class SessionGameRoundUtteranceFactory(object):
-	@unique
-	class EventColumn(Enum):
-		ENTITY_ID = "ENTITY"
-		EVENT_ID = "EVENT"
-		EVENT_NAME = "NAME"
-		EVENT_SUBMITTER = "SUBMITTER"
-		EVENT_TIME = "TIME"
-		ROUND_ID = "ROUND"
+@unique
+class EventColumn(Enum):
+	ENTITY_ID = "ENTITY"
+	EVENT_ID = "EVENT"
+	EVENT_NAME = "NAME"
+	EVENT_SUBMITTER = "SUBMITTER"
+	EVENT_TIME = "TIME"
+	ROUND_ID = "ROUND"
 
+
+class RoundUtteranceSequenceFactory(object):
+	def __init__(self, df: pd.DataFrame, utts: Iterable[utterances.Utterance]):
+		self.df = df
+		self.utts = utts
+		self.cache = {}
+
+	def __call__(self, row: pd.Series):
+		round_id = row[EventColumn.ROUND_ID.value]
+		try:
+			result = self.cache[round_id]
+		except KeyError:
+			result = tuple(self.__find_round_utts(round_id, row))
+			self.cache[round_id] = result
+		return result
+
+	def __find_round_utts(self, round_id: Integral, row: pd.Series) -> Iterator[utterances.Utterance]:
+		start_time = row[EventColumn.EVENT_TIME.value]
+		following_round_events = self.df.loc[(self.df[EventColumn.ROUND_ID.value] > round_id)]
+		end_time = np.inf if len(following_round_events) < 1 else following_round_events[
+			EventColumn.EVENT_TIME.value].min()
+		return (utt for utt in self.utts if start_time <= utt.start_time < end_time)
+
+
+class SessionGameRoundUtteranceFactory(object):
 	UTTERANCE_SEQUENCE_COL_NAME = "UTTERANCES"
+
+	@staticmethod
+	def __first_events(df: pd.DataFrame) -> pd.DataFrame:
+		min_event_time = df.loc[:, EventColumn.EVENT_TIME.value].min()
+		return df.loc[df[EventColumn.EVENT_TIME.value] == min_event_time]
+
+	@staticmethod
+	def __round_utterances(cls, row: pd.Series, df: pd.DataFrame, utts: Iterable[utterances.Utterance]) -> Iterator[
+		utterances.Utterance]:
+		round_id = row[EventColumn.ROUND_ID.value]
+		start_time = row[EventColumn.EVENT_TIME.value]
+		following_round_events = df.loc[(df[EventColumn.ROUND_ID.value] > round_id)]
+		end_time = following_round_events[EventColumn.EVENT_TIME.value].min()
+		return (utt for utt in utts if start_time <= utt.start_time < end_time)
 
 	@staticmethod
 	def __username_participant_ids(usernames: np.ndarray, initial_participant_username: str) -> Iterator[
@@ -38,17 +76,12 @@ class SessionGameRoundUtteranceFactory(object):
 
 	@classmethod
 	def __anonymize_event_submitter_ids(cls, event_df: pd.DataFrame, initial_participant_username: str):
-		event_submitter_ids = event_df[cls.EventColumn.EVENT_SUBMITTER.value]
+		event_submitter_ids = event_df[EventColumn.EVENT_SUBMITTER.value]
 		username_participant_ids = dict(
 			cls.__username_participant_ids(event_submitter_ids.unique(), initial_participant_username))
 		anonymized_event_submitter_ids = event_submitter_ids.transform(
 			lambda submitter_id: username_participant_ids[submitter_id])
-		event_df[cls.EventColumn.EVENT_SUBMITTER.value] = anonymized_event_submitter_ids
-
-	@classmethod
-	def __first_events(cls, df: pd.DataFrame) -> pd.DataFrame:
-		min_event_time = df.loc[:, cls.EventColumn.EVENT_TIME.value].min()
-		return df.loc[df[cls.EventColumn.EVENT_TIME.value] == min_event_time]
+		event_df[EventColumn.EVENT_SUBMITTER.value] = anonymized_event_submitter_ids
 
 	def __init__(self, token_seq_factory: Callable[[Iterable[str]], Sequence[str]]):
 		self.__token_seq_factory = token_seq_factory
@@ -61,47 +94,31 @@ class SessionGameRoundUtteranceFactory(object):
 		event_df = event_data.events
 		self.__anonymize_event_submitter_ids(event_df, event_data.initial_instructor_id)
 		event_df.sort_values(
-			[self.EventColumn.ROUND_ID.value, self.EventColumn.EVENT_ID.value, self.EventColumn.EVENT_TIME.value,
-			 self.EventColumn.ENTITY_ID.value],
+			[EventColumn.ROUND_ID.value, EventColumn.EVENT_ID.value, EventColumn.EVENT_TIME.value,
+			 EventColumn.ENTITY_ID.value],
 			inplace=True)
 
 		# Get the events which describe the referent entity at the time a new turn is submitted
-		turn_submission_events = event_df.loc[event_df[self.EventColumn.EVENT_NAME.value] == "nextturn.request"]
-		round_time_turn_submission_events = turn_submission_events.groupby(self.EventColumn.ROUND_ID.value,
+		turn_submission_events = event_df.loc[event_df[EventColumn.EVENT_NAME.value] == "nextturn.request"]
+		round_time_turn_submission_events = turn_submission_events.groupby(EventColumn.ROUND_ID.value,
 																		   as_index=False)
 		# Ensure the chronologically-first events are chosen (should be unimportant because there should be only one turn submission event per round)
 		round_first_turn_submission_events = round_time_turn_submission_events.apply(self.__first_events)
 
 		# Find the utterances made for each event
 		round_first_turn_submission_event_times = round_first_turn_submission_events.loc[:,
-												  self.EventColumn.EVENT_TIME.value]
+												  EventColumn.EVENT_TIME.value]
 		round_first_turn_submission_event_end_times = itertools.chain(
 			(value for idx, value in round_first_turn_submission_event_times.iteritems()), (np.inf,))
 		segments = utterances.read_segments(session.utts)
 		utts = tuple(seg_utt_factory(segments))
-		round_utts = game_round_utterances(round_first_turn_submission_event_end_times, utts)[1]
-		round_first_turn_submission_events.loc[:, self.UTTERANCE_SEQUENCE_COL_NAME] = round_utts
+		round_utt_seq_factory = RoundUtteranceSequenceFactory(round_first_turn_submission_events, utts)
+		round_first_turn_submission_events.loc[:,
+		self.UTTERANCE_SEQUENCE_COL_NAME] = round_first_turn_submission_events.apply(round_utt_seq_factory, axis=1)
 
-		round_first_turn_submission_events.drop([self.EventColumn.EVENT_ID.value, self.EventColumn.EVENT_NAME.value], 1,
+		round_first_turn_submission_events.drop([EventColumn.EVENT_ID.value, EventColumn.EVENT_NAME.value], 1,
 												inplace=True)
 		# Assert that all entities are represented in each round's set of events
 		assert len(round_first_turn_submission_events) % len(
-			round_first_turn_submission_events[self.EventColumn.ENTITY_ID.value].unique()) == 0
+			round_first_turn_submission_events[EventColumn.ENTITY_ID.value].unique()) == 0
 		return round_first_turn_submission_events
-
-
-def game_round_utterances(round_start_time_iter: Iterator[N],
-						  utts: Iterable[utterances.Utterance]) -> Tuple[Tuple[utterances.Utterance, ...], List[
-	Tuple[utterances.Utterance, ...]]]:
-	currrent_round_start_time = next(round_start_time_iter)
-	# Get utterances preceding the first round
-	pre_game_utts = tuple(utt for utt in utts if utt.start_time < currrent_round_start_time)
-	# TODO: optimize
-	game_round_utts = []
-	for next_round_start_time in round_start_time_iter:
-		current_game_round_utts = tuple(
-			utt for utt in utts if (currrent_round_start_time <= utt.start_time < next_round_start_time))
-		game_round_utts.append(current_game_round_utts)
-		currrent_round_start_time = next_round_start_time
-
-	return pre_game_utts, game_round_utts
