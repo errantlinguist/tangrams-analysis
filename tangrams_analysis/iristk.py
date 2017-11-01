8fd8bbdbb0adea9bcb41fbe204c9db106df9e78b
Todd Shore <tcshore@kth.se>
2017-11-01 11:13:25 +0100

    added additional commentary

--- tangrams_analysis/iristk.py
+++ tangrams_analysis/iristk.py
@@ -4,6 +4,8 @@ import datetime
 def parse_timestamp(date_string: str) -> datetime.datetime:
 	"""""
 	Parses IrisTK-style timestamp strings, i.e. those matching "yyyy-[m]m-[d]d hh:mm:ss[.f...]" with an optional milliseconds part (the ".%f" at the end).
+	
+	See "iristk.system.LoggingModule.logSystemEvent(Event)" in the IrisTK source, which sets the "time" attribute of an event to be broadcast as an instance of "java.sql.Timestamp", the "Object.toString()" method of which produces the aforementioned string format.
 	:param date_string: The timestamp string to parse.
 	:return: A new timestamp object for the given string.
 	:rtype: datetime.datetime
