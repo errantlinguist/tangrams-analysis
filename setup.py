0ba24285c8d90ad3fd35906265e369d1bb0ce583
errantlinguist <errantlinguist+github@gmail.com>
2017-12-03 17:52:06 +0100

    updated license type

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='tangrams_analysis',
       url='https://github.com/errantlinguist/tangrams-analysis',
       author='Todd Shore',
       author_email='errantlinguist+github@gmail.com',
-      license='GPL',
+      license='Apache License, Version 2.0',
       packages=['tangrams_analysis'],
       zip_safe=False)
