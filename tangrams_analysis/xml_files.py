908792210a4b48219cda8d972c3deb137d1b51fc
errantlinguist <errantlinguist+github@gmail.com>
2017-12-03 18:20:34 +0100

    updated email address

--- tangrams_analysis/xml_files.py
+++ tangrams_analysis/xml_files.py
@@ -2,7 +2,7 @@
 Utilities for manipulating XML files.
 """
 
-__author__ = "Todd Shore <tcshore@kth.se>"
+__author__ = "Todd Shore <errantlinguist+github@gmail.com>"
 __copyright__ = "Copyright 2017 Todd Shore"
 __license__ = "Apache License, Version 2.0"
 
