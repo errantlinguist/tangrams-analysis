0044bf824e69412b27e0f0e2ae5b57510222e91a
errantlinguist <errantlinguist+github@gmail.com>
2017-12-03 17:51:20 +0100

    updated email address

--- tangrams_analysis/__init__.py
+++ tangrams_analysis/__init__.py
@@ -2,6 +2,6 @@
 ML and analysis tools for the game \"tangrams-restricted\" <https://github.com/errantlinguist/tangrams-restricted>.
 """
 
-__author__ = "Todd Shore <tcshore@kth.se>"
+__author__ = "Todd Shore <errantlinguist+github@gmail.com>"
 __copyright__ = "Copyright 2017 Todd Shore"
 __license__ = "Apache License, Version 2.0"
