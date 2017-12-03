614b964ee9be8cb7e263be6ad64bfd3fe495e3ad
errantlinguist <errantlinguist+github@gmail.com>
2017-12-03 17:21:35 +0100

    added module documentation

---
+++ tangrams_analysis/__init__.py
@@ -0,0 +1,7 @@
+"""
+ML and analysis tools for the game \"tangrams-restricted\" <https://github.com/errantlinguist/tangrams-restricted>.
+"""
+
+__author__ = "Todd Shore <tcshore@kth.se>"
+__copyright__ = "Copyright 2017 Todd Shore"
+__license__ = "Apache License, Version 2.0"
