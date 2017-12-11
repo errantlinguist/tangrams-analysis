596d6df8ef499d7f4834c8ddfcd3faf1becf063e
Todd Shore <tcshore@kth.se>
2017-12-11 16:26:04 +0100

    implemented counting of tokens in each individual session

--- tangrams_analysis/vocabulary_heatmap.py
+++ tangrams_analysis/vocabulary_heatmap.py
@@ -14,11 +14,22 @@ import argparse
 import sys
 import os
 
+import typing
+from typing import Iterable
+from collections import Counter
+
 import pandas as pd
 
 import utterances
 
 DYAD_ID_COL_NAME = "DYAD"
+
+
+def count_tokens(token_seqs: Iterable[Iterable[str]]) -> typing.Counter[str]:
+    result = Counter()
+    for token_seq in token_seqs:
+        result.update(token_seq)
+    return result
 
 
 def __create_argparser() -> argparse.ArgumentParser:
@@ -48,8 +59,11 @@ def __main(args):
     print("Will read {} file(s).".format(len(inpaths)), file=sys.stderr)
     utt_reader = utterances.UtteranceTabularDataReader()
     utts = pd.concat((__read_utts_file(inpath, utt_reader) for inpath in inpaths))
-    print("Read {} unique utterance(s) from {} file(s) with {} column(s).".format(utts.shape[0], len(inpaths), utts.shape[1]), file=sys.stderr)
-    print(utts)
+    print("Read {} unique utterance(s) from {} file(s) with {} column(s).".format(utts.shape[0], len(inpaths),
+                                                                                  utts.shape[1]), file=sys.stderr)
+    session_utts = utts.groupby(DYAD_ID_COL_NAME)
+    session_token_counts = session_utts[utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value].agg(count_tokens)
+    print(session_token_counts)
 
 
 if __name__ == "__main__":
