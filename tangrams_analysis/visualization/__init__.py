9c145c6aa30db51f71d77f98bf85d79f9159e532
errantlinguist <errantlinguist+github@gmail.com>
2017-12-11 20:37:16 +0100

    created sub-module "visualization"

---
+++ tangrams_analysis/visualization/__init__.py
@@ -0,0 +1,7 @@
+"""
+Scripts for visualizing data.
+"""
+
+__author__ = "Todd Shore <errantlinguist+github@gmail.com>"
+__copyright__ = "Copyright 2017 Todd Shore"
+__license__ = "Apache License, Version 2.0"
