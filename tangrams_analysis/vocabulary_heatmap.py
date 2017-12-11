c068290de39c692ac5257c54b97c425b1a27bb0a
errantlinguist <errantlinguist+github@gmail.com>
2017-12-11 18:26:34 +0100

    cleanup

--- tangrams_analysis/vocabulary_heatmap.py
+++ tangrams_analysis/vocabulary_heatmap.py
@@ -11,13 +11,12 @@ __copyright__ = "Copyright 2017 Todd Shore"
 __license__ = "Apache License, Version 2.0"
 
 import argparse
-import sys
 import os
-
+import sys
 import typing
-from typing import Iterable
 from collections import Counter
 from enum import Enum, unique
+from typing import Iterable
 
 import pandas as pd
 
@@ -26,68 +25,69 @@ import utterances
 
 @unique
 class SessionVocabularyCountDataColumn(Enum):
-    DYAD_ID = "DYAD"
-    TOKEN_TYPE = "TOKEN_TYPE"
-    TOKEN_COUNT = "TOKEN_COUNT"
+	DYAD_ID = "DYAD"
+	TOKEN_TYPE = "TOKEN_TYPE"
+	TOKEN_COUNT = "TOKEN_COUNT"
 
 
 __SESSION_WORD_COUNT_DF_COLS = (
-    SessionVocabularyCountDataColumn.DYAD_ID.value, SessionVocabularyCountDataColumn.TOKEN_TYPE.value,
-    SessionVocabularyCountDataColumn.TOKEN_COUNT.value)
+	SessionVocabularyCountDataColumn.DYAD_ID.value, SessionVocabularyCountDataColumn.TOKEN_TYPE.value,
+	SessionVocabularyCountDataColumn.TOKEN_COUNT.value)
 assert len(__SESSION_WORD_COUNT_DF_COLS) == len(SessionVocabularyCountDataColumn)
 
+
 def count_tokens(token_seqs: Iterable[Iterable[str]]) -> typing.Counter[str]:
-    result = Counter()
-    for token_seq in token_seqs:
-        result.update(token_seq)
-    return result
+	result = Counter()
+	for token_seq in token_seqs:
+		result.update(token_seq)
+	return result
 
 
 def create_session_word_count_df(utts: pd.DataFrame) -> pd.DataFrame:
-    session_utts = utts.groupby(SessionVocabularyCountDataColumn.DYAD_ID.value)
-    session_token_counts = session_utts[utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value].agg(count_tokens)
-    session_word_count_rows = []
-    for session, token_counts in session_token_counts.iteritems():
-        for token_type, count in token_counts.items():
-            session_word_count_rows.append((session, token_type, count))
-    return pd.DataFrame(data=session_word_count_rows, columns=__SESSION_WORD_COUNT_DF_COLS)
+	session_utts = utts.groupby(SessionVocabularyCountDataColumn.DYAD_ID.value)
+	session_token_counts = session_utts[utterances.UtteranceTabularDataColumn.TOKEN_SEQ.value].agg(count_tokens)
+	session_word_count_rows = []
+	for session, token_counts in session_token_counts.iteritems():
+		for token_type, count in token_counts.items():
+			session_word_count_rows.append((session, token_type, count))
+	return pd.DataFrame(data=session_word_count_rows, columns=__SESSION_WORD_COUNT_DF_COLS)
 
 
 def __create_argparser() -> argparse.ArgumentParser:
-    result = argparse.ArgumentParser(
-        description="Visualizes usage of vocabulary across sessions as a heatmap.")
-    result.add_argument("inpaths", metavar="INPATH", nargs='+',
-                        help="The files to process.")
+	result = argparse.ArgumentParser(
+		description="Visualizes usage of vocabulary across sessions as a heatmap.")
+	result.add_argument("inpaths", metavar="INPATH", nargs='+',
+						help="The files to process.")
 
-    return result
+	return result
 
 
 def __dyad_id(infile: str) -> str:
-    session_dir = os.path.dirname(infile)
-    return os.path.split(session_dir)[1]
+	session_dir = os.path.dirname(infile)
+	return os.path.split(session_dir)[1]
 
 
 def __read_utts_file(inpath: str, utt_reader: utterances.UtteranceTabularDataReader) -> pd.DataFrame:
-    dyad_id = __dyad_id(inpath)
-    print("Reading utt file at \"{}\" as dyad \"{}\"".format(inpath, dyad_id), file=sys.stderr)
-    result = utt_reader(inpath)
-    result[SessionVocabularyCountDataColumn.DYAD_ID.value] = dyad_id
-    return result
+	dyad_id = __dyad_id(inpath)
+	print("Reading utt file at \"{}\" as dyad \"{}\"".format(inpath, dyad_id), file=sys.stderr)
+	result = utt_reader(inpath)
+	result[SessionVocabularyCountDataColumn.DYAD_ID.value] = dyad_id
+	return result
 
 
 def __main(args):
-    inpaths = args.inpaths
-    print("Will read {} file(s).".format(len(inpaths)), file=sys.stderr)
-    utt_reader = utterances.UtteranceTabularDataReader()
-    utts = pd.concat((__read_utts_file(inpath, utt_reader) for inpath in inpaths))
-    print("Read {} unique utterance(s) from {} file(s) with {} column(s).".format(utts.shape[0], len(inpaths),
-                                                                                  utts.shape[1]), file=sys.stderr)
-    session_word_count_df = create_session_word_count_df(utts)
-    session_word_count_df.sort_values(
-        [SessionVocabularyCountDataColumn.DYAD_ID.value, SessionVocabularyCountDataColumn.TOKEN_TYPE.value],
-        inplace=True)
-    print(session_word_count_df)
+	inpaths = args.inpaths
+	print("Will read {} file(s).".format(len(inpaths)), file=sys.stderr)
+	utt_reader = utterances.UtteranceTabularDataReader()
+	utts = pd.concat((__read_utts_file(inpath, utt_reader) for inpath in inpaths))
+	print("Read {} unique utterance(s) from {} file(s) with {} column(s).".format(utts.shape[0], len(inpaths),
+																				  utts.shape[1]), file=sys.stderr)
+	session_word_count_df = create_session_word_count_df(utts)
+	session_word_count_df.sort_values(
+		[SessionVocabularyCountDataColumn.DYAD_ID.value, SessionVocabularyCountDataColumn.TOKEN_TYPE.value],
+		inplace=True)
+	print(session_word_count_df)
 
 
 if __name__ == "__main__":
-    __main(__create_argparser().parse_args())
+	__main(__create_argparser().parse_args())
