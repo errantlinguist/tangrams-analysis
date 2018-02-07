2141ff28c341a042653ee9069601772ef2036993
Todd Shore <tcshore@kth.se>
2018-02-07 09:52:19 +0100

    added seaborn plotting logic; Now able to walk through paths supplied by CLI

--- plot_cross_validation_training_weights.py
+++ plot_cross_validation_training_weights.py
@@ -2,8 +2,6 @@
 
 """
 Creates a scatter plot of cross-validation rankings for different update training weights.
-
-Use with e.g. "find ~/Documents/Projects/Tangrams/Data/output/ -iname "*bothspkr*.tsv" -exec ./plot_cross_validation_training_weights.py {} +"
 """
 
 __author__ = "Todd Shore <errantlinguist+github@gmail.com>"
@@ -12,9 +10,14 @@ __license__ = "Apache License, Version 2.0"
 
 import argparse
 import csv
+import os
+import re
 import sys
+from typing import Iterable, Pattern
 
 import pandas as pd
+# Matplotlib for additional customization
+import seaborn as sns
 
 from tangrams_analysis import natural_keys
 
@@ -23,6 +26,21 @@ RESULTS_FILE_CSV_DIALECT = csv.excel_tab
 # NOTE: "category" dtype doesn't work with pandas-0.21.0 but does with pandas-0.21.1
 __RESULTS_FILE_DTYPES = {"DYAD": "category", "IS_TARGET": bool, "IS_OOV": bool,
 						 "IS_INSTRUCTOR": bool, "SHAPE": "category", "ONLY_INSTRUCTOR": bool, "WEIGHT_BY_FREQ": bool}
+
+
+def read_results_files(inpaths: Iterable[str], pattern: Pattern, encoding: str) -> pd.DataFrame:
+	dfs = []
+	for inpath in inpaths:
+		if os.path.isdir(inpath):
+			for root, dirnames, filenames in os.walk(inpath, followlinks=True):
+				for filename in filenames:
+					if pattern.match(filename):
+						abs_path = os.path.join(root, filename)
+						dfs.append(read_results_file(abs_path, encoding))
+		else:
+			# Don't check if the file matches the pattern for directly-specified files
+			dfs.append(read_results_file(inpath, encoding))
+	return pd.concat(dfs)
 
 
 def read_results_file(inpath: str, encoding: str) -> pd.DataFrame:
@@ -40,20 +58,41 @@ def __create_argparser() -> argparse.ArgumentParser:
 						help="The cross-validation results files to process.")
 	result.add_argument("-e", "--encoding", metavar="CODEC", default="utf-8",
 						help="The input file encoding.")
+	result.add_argument("-p", "--pattern", metavar="REGEX", type=re.compile, default=re.compile(".+\.tsv"),
+						help="A regular expression to match the desired files.")
+	result.add_argument("-o", "--outfile", metavar="OUTFILE", required=True,
+						help="The path to write the plot graphics to.")
 	return result
 
 
 def __main(args):
 	infiles = args.infiles
+	pattern = args.pattern
 	encoding = args.encoding
-	print("Will read {} cross-validation results file(s) using encoding \"{}\".".format(len(infiles), encoding),
+	print("Will search {} paths(s) for files matching \"{}\", to be read using encoding \"{}\".".format(len(infiles),
+																										pattern.pattern,
+																										encoding),
 		  file=sys.stderr)
-	cv_results = pd.concat((read_results_file(infile, encoding) for infile in infiles))
+	cv_results = read_results_files(infiles, pattern, encoding)
 	print("Read results for {} iteration(s) of {}-fold cross-validation (one for each dyad).".format(
 		cv_results["CROSS_VALIDATION_ITER"].nunique(), cv_results["DYAD"].nunique()), file=sys.stderr)
 	print("Dyads present: {}".format(sorted(cv_results["DYAD"].unique(), key=natural_keys)), file=sys.stderr)
 	print("Unique discounting values: {}".format(sorted(cv_results["DISCOUNT"].unique())), file=sys.stderr)
 	print("Unique updating weights: {}".format(sorted(cv_results["UPDATE_WEIGHT"].unique())), file=sys.stderr)
+	# groups = cv_results.groupby(("ROUND", "UPDATE_WEIGHT"))
+	# print(groups["RANK"].mean())
+
+	sns.set_style("whitegrid")
+	sns_plot = sns.lmplot(x="ROUND", y="RANK", hue="UPDATE_WEIGHT", data=cv_results)
+	# https://stackoverflow.com/a/39482402/1391325
+	# fig = sns_plot.get_figure()
+	outfile = args.outfile
+	ext = os.path.splitext(outfile)[1][1:]
+	print("Writing to \"{}\" as format \"{}\".".format(outfile, ext), file=sys.stderr)
+	sns_plot.savefig(outfile, format=ext, dpi=1000)
+
+
+# fig.savefig(...)
 
 
 if __name__ == "__main__":
