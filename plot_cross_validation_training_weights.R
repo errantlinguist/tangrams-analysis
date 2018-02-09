ecc897c52e459674ca157946e0e8631e956d2361
Todd Shore <tcshore@kth.se>
2018-02-09 16:27:19 +0100

    changed column name

--- plot_cross_validation_training_weights.R
+++ plot_cross_validation_training_weights.R
@@ -18,13 +18,15 @@ updating_df <- read_results(file.path(indir, "Update1.0-bothspkr.tsv"))
 df <- rbind(baseline_df, updating_df)
 df$RR <- 1.0 / df$RANK
 #df$UPDATE_WEIGHT <- ifelse(df$UPDATE_WEIGHT > 0, "yes", "no")
+# Hack to change legend label
+names(df)[names(df) == "UPDATE_WEIGHT"] <- "Weight"
 
 library(lme4)
 model <- lmer(RR ~ ROUND + BACKGROUND_DATA_WORD_TOKEN_COUNT + INTERACTION_DATA_WORD_TOKEN_COUNT + (1|DYAD), data = df)
 
 #refLevel <- 0
 #Set the reference level for Training
-#relevel(df$UPDATE_WEIGHT, ref=refLevel) -> cvResults$UPDATE_WEIGHT
+#relevel(df$Weight, ref=refLevel) -> cvResults$Weight
 
 df$fit <- predict(model)   #Add model fits to dataframe
 model
@@ -32,8 +34,7 @@ model
 # The palette with black:
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-# + scale_color_manual(values=c("#000000", "#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"))
-plot <- ggplot(df, aes(x=ROUND, y=RR, group=UPDATE_WEIGHT, shape=UPDATE_WEIGHT, color=UPDATE_WEIGHT, linetype=UPDATE_WEIGHT)) + geom_jitter(alpha = 0.5)
+plot <- ggplot(df, aes(x=ROUND, y=RR, group=Weight, shape=Weight, color=Weight, linetype=Weight)) + geom_jitter(alpha = 0.5)
 plot <- plot + geom_smooth(method="lm")
 plot + xlab("Rank") + ylab("RR") + theme_bw() + theme(text=element_text(family="Times")) + scale_colour_manual(values=cbbPalette)
 
