b54c63bf659b55677e3b7c3b21ecd30f2be8ca16
errantlinguist <errantlinguist+github@gmail.com>
2018-02-18 22:06:42 +0100

    changed table float definition

--- plot_round_rr.R
+++ plot_round_rr.R
@@ -66,7 +66,7 @@ relevel(df$Condition, ref=refLevel) -> df$Condition
 
 model <- lmer(RR ~ Condition + poly(round, 2) + (1|Dyad), data = df, REML=TRUE)
 summary(model)
-texreg(model, digits=3, float.pos="htb", single.row=TRUE)
+texreg(model, digits=3, float.pos="!htb", single.row=TRUE)
 
 plot <- ggplot(df, aes(x=round, y=RR, group=Condition, shape=Condition, color=Condition, linetype=Condition)) 
 plot <- plot + stat_summary_bin(fun.data = mean_se, alpha=0.8, size=0.3)
