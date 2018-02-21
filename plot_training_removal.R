6ed22feb6d4f0660dc5781944b3aeae89c42eb86
errantlinguist <errantlinguist+github@gmail.com>
2018-02-22 00:48:50 +0100

    created script for plotting training set size

---
+++ plot_training_removal.R
@@ -0,0 +1,139 @@
+#!/usr/bin/env Rscript
+
+# Plots RR for leaving out training data.
+#
+#
+# Copyright 2018 Todd Shore
+#
+#	Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+args <- commandArgs(trailingOnly=TRUE)
+if(length(args) < 2)
+{
+  stop("Usage: <scriptname> INFILE OUTFILE")
+}
+
+infile = "~/Projects/tangrams-restricted/Data/Analysis/removal.csv"
+#infile <- args[1]
+if (!file_test("-f", infile)) {
+  stop(sprintf("No file found at \"%s\".", infile));
+}
+
+outfile <- args[2]
+
+library(ggplot2)
+library(tools)  
+
+if (!require("viridis")) {
+  if (!require("devtools")) install.packages("devtools")
+  devtools::install_github("sjmgarnier/viridis")
+  library(viridis)
+}
+
+read_results <- function(inpath) {
+  #return(read.xlsx2(inpath, 1, colClasses = c(cond="factor", sess="factor", round="integer", rank="integer", mrr="numeric", accuracy="integer")))
+  return(read.csv(inpath, sep = "\t", colClasses = c(Condition="factor", Dyad="factor", round="integer", rank="integer", removal="integer")))
+  #return(read.csv(inpath, sep = "\t", colClasses=c(DYAD="factor", ONLY_INSTRUCTOR="logical", WEIGHT_BY_FREQ="logical", UPDATE_WEIGHT="factor")))
+}
+
+std.error <- function(x) {
+  # https://www.rdocumentation.org/packages/plotrix/versions/3.7/topics/std.error
+  return(sd(x)/sqrt(sum(!is.na(x))))
+}
+
+# https://stackoverflow.com/a/27694724
+try(windowsFonts(Times=windowsFont("Times New Roman")))
+
+print(sprintf("Reading data from \"%s\".", infile), quote=FALSE)
+df <- read_results(infile)
+sapply(df, class)
+df$RR <- 1.0 / df$rank
+df$TrainingSet <- as.character(df$removal)
+df$TrainingSet <- as.numeric(df$TrainingSet)
+df$TrainingSet <- 32 - as.numeric(df$TrainingSet)
+#df$TrainingSet <- factor(df$TrainingSet)
+df$Condition <- reorder(df$Condition, df$RR, FUN=mean)
+df$Dyad <- factor(df$Dyad)
+# https://stackoverflow.com/a/15665536
+df$Dyad <- factor(df$Dyad, levels = paste(sort(as.integer(levels(df$Dyad)))))
+sapply(df, class)
+#refLevel <- "Baseline"
+# Set the reference level
+#relevel(df$Condition, ref=refLevel) -> df$Condition
+
+rank_digits <- 5
+print(sprintf("Printing ranks rounded to %d significant figures.", rank_digits), quote=FALSE)
+
+print("Condition avg rank:", quote=FALSE)
+print(aggregate(rank ~ Condition, data = df, FUN = mean), short=FALSE, digits=rank_digits)
+print("Condition avg rank standard deviation:", quote=FALSE)
+print(aggregate(rank ~ Condition, data = df, FUN = sd), short=FALSE, digits=rank_digits)
+print("Condition avg rank standard error:", quote=FALSE)
+print(aggregate(rank ~ Condition, data = df, FUN = std.error), short=FALSE, digits=rank_digits)
+
+mrr_digits <- 4
+print(sprintf("Printing MRR rounded to %d significant figures.", mrr_digits), quote=FALSE)
+
+print("Condition MRR:", quote=FALSE)
+print(aggregate(RR ~ Condition, data = df, FUN = mean), short=FALSE, digits=mrr_digits)
+print("Condition MRR standard deviation:", quote=FALSE)
+print(aggregate(RR ~ Condition, data = df, FUN = sd), short=FALSE, digits=mrr_digits)
+print("Condition MRR standard error:", quote=FALSE)
+print(aggregate(RR ~ Condition, data = df, FUN = std.error), short=FALSE, digits=mrr_digits)
+
+plot <- ggplot(df, aes(x=TrainingSet, y=RR))
+plot <- plot + xlab("Background data size (dialogues) ") + ylab("Mean RR")
+aspectRatio <- 2/3
+plot <- plot + theme_light() + theme(text=element_text(family="Times"), aspect.ratio=aspectRatio, plot.margin=margin(4,0,0,0), legend.background=element_rect(fill=alpha("white", 0.0)), legend.box="horizontal", legend.box.margin=margin(0,0,0,0), legend.box.spacing=unit(1, "mm"), legend.direction="horizontal", legend.margin=margin(0,0,0,0), legend.justification = c(0.99, 0.01), legend.position = c(0.99, 0.01), legend.text=element_text(family="mono", face="bold"), legend.title=element_blank()) 
+plot <- plot + scale_color_viridis(discrete=TRUE, option="viridis") + scale_shape_manual(values=1:nlevels(df$Dyad))
+
+baseline_mrr <- 0.6472291648
+#updating_mrr <- 0.6925557115
+plot <- plot + geom_hline(aes(yintercept = baseline_mrr, linetype="Baseline, 32 dialogues"), color="darkred", size=0.7) + scale_linetype_manual(values=c("dashed"))
+
+plot <- plot + stat_summary(fun.data = mean_se, size=0.3, aes(group=Condition, color=Condition, shape=Condition))
+agg_mrrs <- aggregate(RR ~ TrainingSet + Condition, data = df, FUN = mean)
+plot <- plot + geom_line(data=agg_mrrs, aes(group=Condition, color=Condition))
+#plot <- plot + geom_point(aes(group=Condition, color=Condition, shape=Condition))
+#plot <- plot + geom_jitter(alpha = 0.3, size=0.1)
+#plot <- plot + geom_smooth(method="loess", level=0.95, fullrange=TRUE, size=0.7, alpha=0.2)
+#regressionAlpha <- 1.0 / nlevels(df$Condition)
+#regressionAlpha <- 0.333333
+#print(sprintf("Using alpha transparency = %f for each individual regression line.", regressionAlpha), quote=FALSE)
+plot <- plot + geom_smooth(method = "lm", formula = y ~ x, level=0.95, fullrange=TRUE, size=0.7, aes(group=Condition, color=Condition))
+plot <- plot + scale_x_continuous(breaks=sort(unique(df$TrainingSet)))
+#xmin <- min(df$removal)
+#xmax <- round(max(df$removal), digits = -1)
+#xmax <- max(df$removal)
+#ymin <- min(round_mrrs)
+ymin <- 0.6
+ymax = 0.9
+plot <- plot + coord_cartesian(ylim = c(ymin, ymax), expand = FALSE)
+#plot <- plot + scale_x_continuous(limits=c(xmin, xmax), expand = c(0, 0), breaks = scales::pretty_breaks(n = 5)) + scale_y_continuous(limits=c(ymin, 1.0), expand = c(0, 0))
+#plot
+
+output_device <- file_ext(outfile)
+print(sprintf("Writing plot to \"%s\" using format \"%s\".", outfile, output_device), quote=FALSE)
+
+width <- 100
+height <- width * aspectRatio
+ggsave(outfile, plot = plot, device=output_device, width = width, height = height, units="mm", dpi=1000)
+
+
+# https://stackoverflow.com/a/31095291
+#ggplot(tempEf,aes(TRTYEAR, r, group=interaction(site, Myc), col=site, shape=Myc )) + 
+#  facet_grid(~N) +
+#  geom_line(aes(y=fit, lty=Myc), size=0.8) +
+#  geom_point(alpha = 0.3) + 
+#  geom_hline(yintercept=0, linetype="dashed") +
+#  theme_bw()
