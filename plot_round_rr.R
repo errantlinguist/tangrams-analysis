ef8697037ab75eda8b420c0dffac1063aeeb99e4
Todd Shore <tcshore@kth.se>
2018-03-01 13:44:41 +0100

    added extra debug string

--- plot_round_rr.R
+++ plot_round_rr.R
@@ -24,6 +24,7 @@ if(length(args) < 2)
 }
 
 #infile <- "D:\\Users\\tcshore\\Documents\\Projects\\Tangrams\\Data\\Analysis\\update-weight-3-onlyupdating.tsv"
+#infile <- "D:\\Users\\tcshore\\Documents\\Projects\\Tangrams\\Data\\Analysis\\update-weight-3-onlyweighting.tsv"
 #infile <- "~/Projects/tangrams-restricted/Data/Analysis/update-weight-3-onlyupdating.tsv"
 infile <- args[1]
 if (!file_test("-f", infile)) {
