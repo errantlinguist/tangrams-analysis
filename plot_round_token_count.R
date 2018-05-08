538b5ab9d9cbdd1c92d884886ed1149803b5c48a
errantlinguist <errantlinguist+github@gmail.com>
2018-05-07 20:40:34 -0400

    updated round token count plot script for new cross-validated data from 2018-04-27

--- plot_round_token_count.R
+++ plot_round_token_count.R
@@ -23,7 +23,7 @@ if(length(args) < 2)
   stop("Usage: <scriptname> INFILE OUTFILE")
 }
 
-#infile <- "D:\\Users\\tcshore\\Documents\\Projects\\Tangrams\\Data\\Analysis\\rounds_length.tsv"
+#infile <- "~/Projects/tangrams-restricted/Data/Analysis/2018-04-27/results-cross-2.tsv"
 infile <- args[1]
 if (!file_test("-f", infile)) {
   stop(sprintf("No file found at \"%s\".", infile));
@@ -32,39 +32,41 @@ if (!file_test("-f", infile)) {
 outfile <- args[2]
 
 library(ggplot2)
-library(tools)  
-
-if (!require("viridis")) {
-  if (!require("devtools")) install.packages("devtools")
-  devtools::install_github("sjmgarnier/viridis")
-  library(viridis)
-}
+library(tools)
 
 read_results <- function(inpath) {
-  return(read.csv(inpath, sep = "\t", colClasses = c(session="factor", round="integer", rank="integer", length="integer")))
+  #return(read.csv(inpath, sep = "\t", colClasses = c(session="factor", round="integer", rank="integer", length="integer")))
+  return(read.csv(inpath, sep = "\t", colClasses = c(cond="factor", session="factor", round="integer")))
 }
 
 # https://stackoverflow.com/a/27694724
 try(windowsFonts(Times=windowsFont("Times New Roman")))
 
 df <- read_results(infile)
+sapply(df, class)
+# Only select the rows using just "Baseline"
+df <- df[df$cond %in% c("Baseline"), ]
 # Hack to change legend label
+names(df)[names(df) == "cond"] <- "Condition"
+names(df)[names(df) == "rank"] <- "Rank"
+names(df)[names(df) == "round"] <- "Round"
 names(df)[names(df) == "session"] <- "Dyad"
+names(df)[names(df) == "weight"] <- "RA"
+names(df)[names(df) == "words"] <- "Tokens"
 # https://stackoverflow.com/a/15665536
 df$Dyad <- factor(df$Dyad, levels = paste(sort(as.integer(levels(df$Dyad)))))
 
 
-#aggs <- aggregate(RA ~ Dyad * round, df, mean)
+#aggs <- aggregate(RA ~ Dyad * Round, df, mean)
 
-plot <- ggplot(df, aes(x=round, y=length))
+plot <- ggplot(df, aes(x=Round, y=Tokens))
 
 #text(4, 7, expression(bar(x) == sum(frac(x[i], n), i==1, n)))
 plot <- plot + xlab(expression(paste("Game round ", italic("i")))) + ylab("Token count")
 aspectRatio <- 3/4
-plot <- plot + theme_light() + theme(text=element_text(family="Times"), aspect.ratio=aspectRatio, plot.margin=margin(4,0,0,0), legend.background=element_rect(fill=alpha("white", 0.0)), legend.box.margin=margin(0,0,0,0), legend.box.spacing=unit(1, "mm"), legend.direction="horizontal", legend.margin=margin(0,0,0,0), legend.justification = c(0.99, 0.99), legend.position = c(0.99, 0.99), legend.text=element_text(family="mono", face="bold"))
-plot <- plot + scale_color_viridis(discrete=TRUE, option="viridis") + scale_shape_manual(values=1:nlevels(df$Dyad))
+plot <- plot + theme_light() + theme(text=element_text(family="Times"), aspect.ratio=aspectRatio, plot.margin=margin(4,0,0,0), legend.background=element_rect(fill=alpha("white", 0.0)))
 
-plot <- plot + geom_point(aes(color=Dyad, shape=Dyad))
+plot <- plot + stat_summary(fun.data = mean_se, size=0.01)
 #plot <- plot + geom_line()
 #plot
 #plot <- plot + geom_jitter(alpha = 0.3, size=0.1)
@@ -75,12 +77,12 @@ plot <- plot + geom_smooth(method = "lm", formula = y ~ poly(x,2), level=0.95, f
 #plot <- plot + geom_smooth(method = "lm", formula = y ~ x, level=0.95, fullrange=TRUE, size=0.7, aes(color=Dyad))
 plot <- plot + scale_y_log10()
 
-xmin <- min(df$round)
-xmax <- max(df$round)
-#round_mra <- aggregate(MRA ~ round, data = df, FUN = mean)
-ymin <- min(df$length)
-ymax <- max(df$length)
-#ymax <- 150
+xmin <- min(df$Round)
+xmax <- max(df$Round)
+#round_mra <- aggregate(MRA ~ Round, data = df, FUN = mean)
+ymin <- min(df$Tokens)
+ymax <- max(df$Tokens)
+ymax <- 100
 plot <- plot + coord_cartesian(xlim = c(xmin, xmax), ylim = c(ymin, ymax), expand = FALSE)
 #plot <- plot + scale_x_continuous(limits=c(xmin, xmax), expand = c(0, 0), breaks = scales::pretty_breaks(n = 5)) + scale_y_continuous(limits=c(ymin, 1.0), expand = c(0, 0))
 #plot
